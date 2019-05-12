7cdcafc7d85e2e68592e7321b6fd7604303c2614
Stream <zsjdxc251@live.com>
2019-05-12 22:31:01 +0800

    api

--- example-spring-cloud-eureka/src/main/java/com/lesson/cloud/eureka/EurekaServerBootstrap.java
+++ example-spring-cloud-eureka/src/main/java/com/lesson/cloud/eureka/EurekaServerBootstrap.java
@@ -2,11 +2,13 @@ package com.lesson.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author zhengshijun
  * @version created on 2019/5/11.
  */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerBootstrap {
 
