19472b9ac561b7b9d505a385f778016d6b14da90
Stream <zsjdxc251@live.com>
2019-05-12 16:55:35 +0800

    gradle 调整

---
+++ example-spring-cloud-feign-consumer/src/main/java/com/lesson/cloud/feign/consumer/FeignConsumerBootstrap.java
@@ -0,0 +1,17 @@
+package com.lesson.cloud.feign.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhengshijun
+ * @version created on 2019/5/12.
+ */
+@SpringBootApplication
+public class FeignConsumerBootstrap {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FeignConsumerBootstrap.class,args);
+    }
+}
