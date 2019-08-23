260862f2c82ede40c730559c955de59a33cda8e4
zhengshijun <zhengsj@akulaku.com>
2019-08-23 20:30:17 +0800

    处理

--- example-spring-cloud-feign-consumer/src/main/java/com/lesson/cloud/feign/consumer/FeignConsumerBootstrap.java
+++ example-spring-cloud-feign-consumer/src/main/java/com/lesson/cloud/feign/consumer/FeignConsumerBootstrap.java
@@ -2,9 +2,12 @@ package com.lesson.cloud.feign.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.serviceregistry.AbstractAutoServiceRegistration;
 import org.springframework.cloud.openfeign.FeignClientProperties;
 
 /**
+ *
+ *  {@link AbstractAutoServiceRegistration#bind(org.springframework.boot.web.context.WebServerInitializedEvent)}
  * @see FeignClientProperties
  * @author zhengshijun
  * @version created on 2019/5/12.
